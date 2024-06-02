9afbb501fd2c0c931fb46a18f1729d9f2d53db43
hasret-turkmen <hasret.turkmen@outlook.com.tr>
2024-06-02 18:57:17 +0300

    added video

--- frontend/src/js/Video.js
+++ frontend/src/js/Video.js
@@ -1,42 +1,23 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../css/Notifications.css'; // Custom button CSS'ini dahil edin
 
 const Video = () => {
-    const [videoUrl, setVideoUrl] = useState('');
     const navigate = useNavigate(); // useNavigate hook'unu ekleyin
-
-    useEffect(() => {
-        const fetchVideo = async () => {
-            try {
-                console.log('Fetching video from http://10.50.11.12:5000/video');
-                const response = await axios.get('http://10.50.11.12:5000/video', {
-                    responseType: 'blob',
-                });
-                console.log('Video fetch successful:', response);
-                const url = URL.createObjectURL(new Blob([response.data]));
-                setVideoUrl(url);
-            } catch (error) {
-                console.error('Error fetching video:', error);
-            }
-        };
-
-        fetchVideo();
-    }, []);
 
     return (
         <div>
             <h1>Video Page</h1>
-            {videoUrl ? (
-                <video width="600" controls>
-                    <source src={videoUrl} type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
-            ) : (
-                <p>Loading video...</p>
-            )}
-            <button className="custom-button" onClick={() => navigate('/welcome')}>Back to Welcome</button>
+            <iframe
+                src="http://10.10.223.207:5000/video"
+                width="1500"
+                height="900"
+                frameBorder="0"
+                allowFullScreen
+            ></iframe>
+            <div>
+                <button className="custom-button" onClick={() => navigate('/welcome')}>Back to Welcome</button>
+            </div>
         </div>
     );
 };
